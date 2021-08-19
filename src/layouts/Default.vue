dd0bbd4220b203bf5456c45d75d9db4263bfb97a
Anticopyright <53624153+dudul-bdul@users.noreply.github.com>
2021-08-19 16:03:06 +0700

    Update Default.vue

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -16,7 +16,10 @@
 
       <a href="https://api.whatsapp.com/send?phone=51955081075&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202." class="float" target="_blank">
         <i class="fa fa-whatsapp my-float"></i>
+        <img src="./images/whatapps.png" alt="Order Via Whatsapp">
       </a>
+
+      <img src="./images/whatapps.png" alt="Order Via Whatsapp">
     </main>
 
     <footer class="footer">
