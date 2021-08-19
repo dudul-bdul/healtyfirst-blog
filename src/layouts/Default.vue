ec57e14e173819a876b0bf02c712beb5e237949b
Anticopyright <53624153+dudul-bdul@users.noreply.github.com>
2021-08-19 14:36:09 +0700

    Update

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -13,6 +13,12 @@
 
     <main class="main">
       <slot/>
+
+      <button class="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button"
+      >
+  <i class="fas fa-heart"></i> Regular
+</button>
+
     </main>
 
     <footer class="footer">
