2b2f7391dbba1cc352a20e9b210d8c40b7928c51
Anticopyright <53624153+dudul-bdul@users.noreply.github.com>
2021-08-19 15:56:49 +0700

    Update

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -13,6 +13,10 @@
 
     <main class="main">
       <slot/>
+
+      <a href="https://api.whatsapp.com/send?phone=51955081075&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202." class="float" target="_blank">
+        <i class="fa fa-whatsapp my-float"></i>
+      </a>
     </main>
 
     <footer class="footer">
@@ -81,5 +85,24 @@ export default {
   a {
     color: currentColor;
   }
+}
+
+.float{
+	position:fixed;
+	width:60px;
+	height:60px;
+	bottom:40px;
+	right:40px;
+	background-color:#25d366;
+	color:#FFF;
+	border-radius:50px;
+	text-align:center;
+  font-size:30px;
+	box-shadow: 2px 2px 3px #999;
+  z-index:100;
+}
+
+.my-float{
+	margin-top:16px;
 }
 </style>
